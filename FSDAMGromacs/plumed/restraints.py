b6e80f5d7fdd081fa0b4e8410a5b876ee41f3659
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2020-11-12 15:27:07 +0100

    function for COM restraint w plumed

---
+++ FSDAMGromacs/plumed/restraints.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+# pylint: disable-msg=too-many-locals
+#############################################################
+# Copyright (c) 2020-2020 Maurice Karrenbrock               #
+#                                                           #
+# This software is open-source and is distributed under the #
+# BSD 3-Clause "New" or "Revised" License                   #
+#############################################################
+"""Functions to introduce restraints with plumed
+"""
+
+import PythonAuxiliaryFunctions.files_IO.write_file as write_file
+
+
+def COM_COM_restraint(atom_groups,
+                      restraint_parameters,
+                      plumed_file='plumed.dat',
+                      distances_file='distances.out',
+                      stride=1,
+                      geometric=False):
+    """create a center of mass (COM) - center of mass restraint with plumed
+
+    creates a plumed input file https://www.plumed.org in order to create
+    a restraint between 2 centers of mass
+
+    Parameters
+    ------------
+    atom_groups : dict
+        {"group_name" : [<list of atoms>], ...} the keys of the dictionary must
+        be the group names (NO SPACES!!!)
+        and the value must be a list containing all atom numbers of the group,
+        there can be as many groups as
+        you like
+    restraint_parameters : list
+        a nested list: [ ["grop_name_1", "grop_name_2", equilibium_distance,
+        harmonic_kappa, linear_slope], ... ]
+        the names must be str the other parameters float, remember that you
+        can always set a certain parameter to zero 0.
+    plumed_file : str
+        the name of the plumed input file to be created
+        (default 'plumed.dat')
+    distances_file : str
+        the name of the file that plumed will create and in which all the distances will be
+        written out (default 'distances.out')
+    stride : int
+        `distances_file` will be updated each `stride` MD steps
+        (default 1)
+    geometric : bool, optional
+        if True instead of the center of mass the geometrical center
+        will be calulated (default False). Can come in handy with strange
+        atoms/residues
+
+    Raises
+    ---------
+    ValueError
+        if `atom_groups` contains only one group
+        but there is no check to see if
+        `restraint_parameters` doesn't contain a value for
+        each possible couple
+    """
+    def make_atoms_string(atoms):
+        """private
+        """
+
+        #build atoms string
+        atom_string = ''
+        for atom in atoms:
+
+            atom_string += f'{atom},'
+
+        #remove last comma
+        atom_string = atom_string[:-1]
+
+        return atom_string
+
+    if len(atom_groups) < 2:
+        raise ValueError(
+            f'need at least 2 atom groups, not {len(atom_groups)}')
+
+    output = []
+
+    if geometric:
+        com = 'CENTER'
+    else:
+        com = 'COM'
+
+    #WHOLEMOLECULES string
+    string = 'WHOLEMOLECULES '
+    for i, name in enumerate(atom_groups.keys()):
+
+        #build atoms string
+        atom_string = make_atoms_string(atom_groups[name])
+
+        string += f'ENTITY{i}={atom_string} '
+
+    string += '\n'
+
+    output.append(string)
+
+    #define centers of mass (COM or CENTER):
+    for name in atom_groups.keys():
+
+        atom_string = make_atoms_string(atom_groups[name])
+
+        string = f'{name}: {com} ATOMS={atom_string}\n'
+
+        output.append(string)
+
+    #define COM-COM distances DISTANCE NOPBC
+    names = list(atom_groups.keys())
+    distances_list = []
+    for i in range(len(names)):
+
+        for j in range(i + 1, len(names)):
+
+            string = f'{names[i]}_{names[j]}_dist: DISTANCE ATOMS={names[i]},{names[j]} NOPBC\n'
+
+            distances_list.append(
+                [f'{names[i]}_{names[j]}_dist', names[i], names[j]])
+
+            output.append(string)
+
+    #create restraints RESTRAINT
+    for couple in restraint_parameters:
+
+        for distance in distances_list:
+
+            if (distance[1]
+                    in (couple[0], couple[1])) and (distance[2]
+                                                    in (couple[0], couple[1])):
+
+                string = \
+                'RESTRAINT ARG={} AT={:.4f} KAPPA={:.4f} SLOPE={:.4f}\n'.format(
+                    distance[0],
+                    couple[2],
+                    couple[3],
+                    couple[4]
+                )
+
+                output.append(string)
+
+    #PRINT statement
+    string = 'PRINT ARG='
+
+    for distance in distances_list:
+
+        string += distance[0] + ','
+
+    #remove last comma
+    string = string[:-1]
+
+    string += f' STRIDE={stride} '
+
+    string += f'FILE={distances_file}\n'
+
+    output.append(string)
+
+    #write file
+    write_file.write_file(output, plumed_file)
---
+++ tests/unit_tests/test_plumed/test_restraints.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+# pylint: disable=redefined-outer-name
+# pylint: disable=no-self-use
+# pylint: disable=protected-access
+#############################################################
+# Copyright (c) 2020-2020 Maurice Karrenbrock               #
+#                                                           #
+# This software is open-source and is distributed under the #
+# BSD 3-Clause "New" or "Revised" License                   #
+#############################################################
+
+import pytest
+
+import FSDAMGromacs.plumed.restraints as restraints
+
+
+class TestCOM_COM_restraint():
+    def test_raises(self):
+
+        with pytest.raises(ValueError):
+
+            restraints.COM_COM_restraint({'aaa': [1, 2]}, [['DUM']])
+
+    @pytest.mark.parametrize('test_type, geometric, com',
+                             [('geometric_false', False, 'COM'),
+                              ('geometric_true', True, 'CENTER')])
+    def test_works(self, mocker, test_type, geometric, com):
+
+        print('Logging test type for visibility: ' + test_type)
+
+        m_write = mocker.patch(
+            'PythonAuxiliaryFunctions.files_IO.write_file.write_file')
+
+        atom_groups = {
+            'group_1': [1, 2, 3, 4],
+            'group_2': [5, 6, 7, 8],
+            'group_3': [9, 10, 11, 12]
+        }
+
+        restraint_parameters = [['group_1', 'group_2', 1., 2., 3.],
+                                ['group_1', 'group_3', 4., 5., 6.],
+                                ['group_2', 'group_3', 0.7, 0.8, 0.9]]
+
+        plumed_file = 'test_plumed.dat'
+        distances_file = 'test_distances.out'
+        stride = 10
+
+        expected = [
+            'WHOLEMOLECULES ENTITY0=1,2,3,4 ENTITY1=5,6,7,8 ENTITY2=9,10,11,12 \n',
+            f'group_1: {com} ATOMS=1,2,3,4\n',
+            f'group_2: {com} ATOMS=5,6,7,8\n',
+            f'group_3: {com} ATOMS=9,10,11,12\n',
+            'group_1_group_2_dist: DISTANCE ATOMS=group_1,group_2 NOPBC\n',
+            'group_1_group_3_dist: DISTANCE ATOMS=group_1,group_3 NOPBC\n',
+            'group_2_group_3_dist: DISTANCE ATOMS=group_2,group_3 NOPBC\n',
+            'RESTRAINT ARG=group_1_group_2_dist AT=1.0000 KAPPA=2.0000 SLOPE=3.0000\n',
+            'RESTRAINT ARG=group_1_group_3_dist AT=4.0000 KAPPA=5.0000 SLOPE=6.0000\n',
+            'RESTRAINT ARG=group_2_group_3_dist AT=0.7000 KAPPA=0.8000 SLOPE=0.9000\n',
+            f'PRINT ARG=group_1_group_2_dist,group_1_group_3_dist,group_2_group_3_dist STRIDE={stride} FILE={distances_file}\n'  # pylint: disable=line-too-long
+        ]
+
+        restraints.COM_COM_restraint(atom_groups, restraint_parameters,
+                                     plumed_file, distances_file, stride,
+                                     geometric)
+
+        m_write.assert_called_once_with(expected, plumed_file)
