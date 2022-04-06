8d8d815391a674eee109a2f21cfc1493395b73a3
MauriceKarrenbrock <56843667+MauriceKarrenbrock@users.noreply.github.com>
2022-04-06 05:34:35 +0200

    added possibility to add custom string to top files

--- FSDAMGromacs/top_files.py
+++ FSDAMGromacs/top_files.py
@@ -12,6 +12,46 @@ import PythonAuxiliaryFunctions.files_IO.read_file as read_file
 import PythonAuxiliaryFunctions.files_IO.write_file as write_file
 
 
+def add_string_after_FF(input_string, input_top_file, output_top_file):
+    """adds a string after the FF one
+
+    In case there is no FF include (es a parmed genereted topology) it will
+    be added after deafults
+
+    Parameters
+    -----------
+    input_string : str
+    input_top_file : str
+    output_top_file : str
+        can be the same as the input one
+    """
+
+    input_top_lines = read_file.read_file(input_top_file)
+
+    for i in range(len(input_top_lines)):
+
+        if input_top_lines[i].strip() != '':
+            if input_top_lines[i].strip()[0] != ';':
+
+                if input_top_lines[i].strip()[0:8] == '#include':
+
+                    input_top_lines[i] += f'\n{input_string}\n'
+
+                    break
+
+                # In case there is no FF include (es a parmed genereted topology)
+                if input_top_lines[i].strip()[0] == '[' and \
+                    input_top_lines[i].split(';')[0].strip().replace(' ', '') != '[defaults]':
+
+                    input_top_lines[
+                        i] = f'\n{input_string}\n' + input_top_lines[
+                            i]
+
+                    break
+
+    write_file.write_file(input_top_lines, output_top_file)
+
+
 def add_include_after_FF(include_line, input_top_file, output_top_file):
     """adds an include statement after the FF one
 
@@ -34,52 +74,21 @@ def add_include_after_FF(include_line, input_top_file, output_top_file):
     itp files
     """
 
-    input_top_lines = read_file.read_file(input_top_file)
-
-    for i in range(len(input_top_lines)):
-
-        if input_top_lines[i].strip() != '':
-            if input_top_lines[i].strip()[0] != ';':
-
-                if input_top_lines[i].strip()[0:8] == '#include':
-
-                    input_top_lines[i] += f'\n#include "{include_line}"\n'
-
-                    break
-
-                # In case there is no FF include (es a parmed genereted topology)
-                if input_top_lines[i].strip()[0] == '[' and \
-                    input_top_lines[i].split(';')[0].strip().replace(' ', '') != '[defaults]':
-
-                    input_top_lines[
-                        i] = f'\n#include "{include_line}"\n' + input_top_lines[
-                            i]
-
-                    break
-
-    write_file.write_file(input_top_lines, output_top_file)
+    add_string_after_FF(f'#include "{include_line}"', input_top_file, output_top_file)
 
 
-def add_include_after_atomtypes(include_line, input_top_file, output_top_file):
-    """adds an include statement after atomtypes
+def add_string_after_atomtypes(input_string, input_top_file, output_top_file):
+    """adds a string after atomtypes
 
     It checks for the beginning of a [ ... ] section that is not [ atomtypes ]
     and for the beginning of a #ifdef
 
     Parameters
     -----------
-    include_line : str
-        the thing to include, DON'T write #include
-        but only the itp file name (or path)
+    input_string : str
     input_top_file : str
     output_top_file : str
         can be the same as the input one
-
-    Notes
-    ----------
-    if you have to include multiple itp files all the [ atomtypes ]
-    must be after the force field include and only then you can add the remaining parts of the
-    itp files
     """
     def is_right_line(line):
         """complex bool expession
@@ -108,16 +117,41 @@ def add_include_after_atomtypes(include_line, input_top_file, output_top_file):
             if is_right_line(input_top_lines[i]):
 
                 input_top_lines[i] = \
-                f'\n#include "{include_line}"\n{input_top_lines[i].strip()}\n'
+                f'\n{input_string}\n{input_top_lines[i].strip()}\n'
 
                 break
 
     # If end of file was reached try to put it in the end
     # as last resort
     else:
-        input_top_lines[-1] += f'\n#include "{include_line}"\n'
+        input_top_lines[-1] += f'\n{input_string}\n'
 
     write_file.write_file(input_top_lines, output_top_file)
+
+
+def add_include_after_atomtypes(include_line, input_top_file, output_top_file):
+    """adds an include statement after atomtypes
+
+    It checks for the beginning of a [ ... ] section that is not [ atomtypes ]
+    and for the beginning of a #ifdef
+
+    Parameters
+    -----------
+    include_line : str
+        the thing to include, DON'T write #include
+        but only the itp file name (or path)
+    input_top_file : str
+    output_top_file : str
+        can be the same as the input one
+
+    Notes
+    ----------
+    if you have to include multiple itp files all the [ atomtypes ]
+    must be after the force field include and only then you can add the remaining parts of the
+    itp files
+    """
+
+    add_string_after_atomtypes(f'#include "{include_line}"', input_top_file, output_top_file)
 
 
 def add_molecules(name, number, input_top_file, output_top_file):
